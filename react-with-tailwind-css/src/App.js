5bdbf6c3e528805ca722506576000d80afda5298
ak2311581 <149038996+ak2311581@users.noreply.github.com>
2024-07-07 07:40:19 +0530

    07-07-2024

--- react-with-tailwind-css/src/App.js
+++ react-with-tailwind-css/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import TypoGraphy from './components/Typography'
+import TypoGraphy from './components/Typography';
+import Layouts from './components/Layout';
 
 function App() {
   return (
     <>
-<TypoGraphy/>
-
+{/* <TypoGraphy/> */}
+      <Layouts/>
     </>
   );
 }
--- react-with-tailwind-css/src/components/Typography/index.js
+++ react-with-tailwind-css/src/components/Typography/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function index(props) {
     return (
        <>
-    <div className="text-center  text-red-700 text-3xl font-sans font-medium tracking-wider leading-10 w-80 h-10 border border-black m-10 overflow-hidden truncate">
+    <div className="text-center  text-red-700 text-4xl font-sans font-bold tracking-wide leading-5 w-80 h-10 border border-black m-10 overflow-hidden truncate">
         Hellow world Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
      </div>
      <hr/>
